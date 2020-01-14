a9da388731145a83acbd76c88951a3bbb9f32381
Vaclav Macha <vaclav.macha92@gmail.com>
2020-01-14 14:47:36 +0100

    Fixe auc

--- src/utilities.jl
+++ src/utilities.jl
@@ -234,21 +234,23 @@ end
 Computes the area under curve `(x,y)` using trapezoidal rule.
 """
 function auc(x::RealVector, y::RealVector)
-    n   = length(x)
-    val = zero(eltype(x))
+    n  = length(x)
     n == length(y) || throw(DimensionMismatch("Inconsistent lengths of `x` and `y`."))
 
     if issorted(x)
-        prm = 1:n
+        prm = 1:n 
+    elseif issorted(x, rev = true)
+        prm = n:-1:1
     else
-        prm = sortperm(x)
+        throw(ArgumentError("x must be sorted."))
     end
 
-    @inbounds for i in 2:n
+    val = zero(promote_type(eltype(x), eltype(y)))
+    for i in 2:n
         Δx   = x[prm[i]]  - x[prm[i-1]]
         Δy   = (y[prm[i]] + y[prm[i-1]])/2
-        
-        if !(isnan(Δx) || isnan(Δy))
+
+        if !(isnan(Δx) || isnan(Δy) || Δx == 0) 
             val += Δx*Δy
         end
     end
