3344c8cb22f40df7232c7e5fc3d051030344b775
VaclavMacha <vaclav.macha92@gmail.com>
2021-12-31 15:23:50 +0100

    Removed label mapping from confusiomatrix

--- src/confusion_matrix.jl
+++ src/confusion_matrix.jl
@@ -3,27 +3,26 @@ using Base: ImmutableDict
 abstract type AbstractConfusionMatrix{I<:Integer} <: AbstractMatrix{I} end
 
 struct ConfusionMatrix{N,T,I<:Integer} <: AbstractConfusionMatrix{I}
+    classes::NTuple{N,T}
     data::Matrix{I}
-    mapping::ImmutableDict{T,I}
 
     function ConfusionMatrix(classes)
         N = length(classes)
-        mapping = ImmutableDict([class => i for (i, class) in enumerate(classes)]...)
-        return new{N,keytype(mapping),Int}(zeros(Int, N, N), mapping)
+        cls = (classes...,)
+        return new{N,eltype(cls),Int}(cls, zeros(Int, N, N))
     end
 end
 
 struct BinaryConfusionMatrix{N,T,I<:Integer} <: AbstractConfusionMatrix{I}
+    classes::NTuple{N,T}
     data::Matrix{I}
-    mapping::ImmutableDict{T,I}
 
     function BinaryConfusionMatrix(classes)
-        N = length(classes)
-        if N != 2
+        if length(classes) != 2
             throw(DimensionMismatch("BinaryConfusionMatrix is defined only for 2D problems: got $(N)D input. Use ConfusionMatrix instead."))
         end
-        mapping = ImmutableDict([class => i for (i, class) in enumerate(classes)]...)
-        return new{N,keytype(mapping),Int}(zeros(Int, N, N), mapping)
+        cls = (classes...,)
+        return new{2,eltype(cls),Int}(cls, zeros(Int, 2, 2))
     end
 end
 
@@ -31,13 +30,17 @@ Base.size(C::AbstractConfusionMatrix) = size(C.data)
 Base.getindex(C::AbstractConfusionMatrix, I::Vararg{Int,2}) = getindex(C.data, I...)
 Base.setindex!(C::AbstractConfusionMatrix, v, I::Vararg{Int,2}) = setindex!(C.data, v, I...)
 
+labelmap(C::AbstractConfusionMatrix) = labelmap(C.classes)
+labelmap(classes) = ImmutableDict(Pair.(classes, eachindex(classes))...)
+
 function fill!(C::AbstractConfusionMatrix, y, ŷ, predict = identity)
     if length(y) != length(ŷ)
         throw(DimensionMismatch("Inconsistent lengths of targets and predictions."))
     end
+    mapping = labelmap(C)
 
     for (yi, ŷi) in zip(y, ŷ)
-        @inbounds C[C.mapping[yi], C.mapping[predict(ŷi)]] += 1
+        @inbounds C[mapping[yi], mapping[predict(ŷi)]] += 1
     end
     return
 end
