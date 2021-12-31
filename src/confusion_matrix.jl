997eb30e90c25137e93a9a546b8f7a44096d00b6
VaclavMacha <vaclav.macha92@gmail.com>
2021-12-31 17:44:55 +0100

    Add definition of + for ConfusionMatrix

--- src/confusion_matrix.jl
+++ src/confusion_matrix.jl
@@ -13,6 +13,19 @@ struct ConfusionMatrix{N,T,I<:Integer} <: AbstractConfusionMatrix{I}
     end
 end
 
+struct BinaryConfusionMatrix{N,T,I<:Integer} <: AbstractConfusionMatrix{I}
+    classes::NTuple{N,T}
+    data::Matrix{I}
+
+    function BinaryConfusionMatrix(classes)
+        if length(classes) != 2
+            throw(DimensionMismatch("BinaryConfusionMatrix is defined only for 2D problems: got $(N)D input. Use ConfusionMatrix instead."))
+        end
+        cls = (classes...,)
+        return new{2,eltype(cls),Int}(cls, zeros(Int, 2, 2))
+    end
+end
+
 function Base.show(io::IO, ::MIME"text/plain", C::AbstractConfusionMatrix)
     N = size(C.data, 2)
     return pretty_table(
@@ -42,29 +55,38 @@ function Base.show(io::IO, ::MIME"text/plain", C::AbstractConfusionMatrix)
                 crayon = Crayon(bold = true, foreground = :green)
             ),
         ),
-        newline_at_end = false,
+        newline_at_end = false
     )
 end
 
-struct BinaryConfusionMatrix{N,T,I<:Integer} <: AbstractConfusionMatrix{I}
-    classes::NTuple{N,T}
-    data::Matrix{I}
-
-    function BinaryConfusionMatrix(classes)
-        if length(classes) != 2
-            throw(DimensionMismatch("BinaryConfusionMatrix is defined only for 2D problems: got $(N)D input. Use ConfusionMatrix instead."))
-        end
-        cls = (classes...,)
-        return new{2,eltype(cls),Int}(cls, zeros(Int, 2, 2))
-    end
-end
-
 Base.size(C::AbstractConfusionMatrix) = size(C.data)
+Base.zero(C::ConfusionMatrix) = ConfusionMatrix(C.classes)
+Base.zero(C::BinaryConfusionMatrix) = ConfusionMatrix(C.classes)
 Base.getindex(C::AbstractConfusionMatrix, I::Vararg{Int,2}) = getindex(C.data, I...)
 Base.setindex!(C::AbstractConfusionMatrix, v, I::Vararg{Int,2}) = setindex!(C.data, v, I...)
 
+function Base.:(+)(C1::AbstractConfusionMatrix, C2::AbstractConfusionMatrix)
+    cls1 = C1.classes
+    cls2 = C2.classes
+
+    if cls1 == cls2
+        C = zero(C1)
+        C.data .+= C1.data .+ C2.data
+        return C
+    elseif issubset(cls1, cls2) && issubset(cls2, cls1)
+        prm = [findfirst(isequal(i), cls2) for i in cls1]
+        C = zero(C1)
+        C.data .+= C1.data .+ C2.data[prm, prm]
+        return C
+    else
+        error("Confusion matrices do not have same classes.")
+    end
+end
+
 labelmap(C::AbstractConfusionMatrix) = labelmap(C.classes)
 labelmap(classes) = ImmutableDict(Pair.(classes, eachindex(classes))...)
+
+
 
 function fill!(C::AbstractConfusionMatrix, y, ŷ, predict = identity)
     if length(y) != length(ŷ)
