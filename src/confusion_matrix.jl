da5ac3310d37d315d9f35f0a8deb7923adde8dfc
VaclavMacha <vaclav.macha92@gmail.com>
2022-01-05 19:43:04 +0100

    Refactored confusion_matrix. Added doc strings.

--- src/confusion_matrix.jl
+++ src/confusion_matrix.jl
@@ -2,29 +2,9 @@ using Base: ImmutableDict
 
 abstract type AbstractConfusionMatrix{I<:Integer} <: AbstractMatrix{I} end
 
-struct ConfusionMatrix{N,T,I<:Integer} <: AbstractConfusionMatrix{I}
-    classes::NTuple{N,T}
-    data::Matrix{I}
-
-    function ConfusionMatrix(classes)
-        N = length(classes)
-        cls = (classes...,)
-        return new{N,eltype(cls),Int}(cls, zeros(Int, N, N))
-    end
-end
-
-struct BinaryConfusionMatrix{N,T,I<:Integer} <: AbstractConfusionMatrix{I}
-    classes::NTuple{N,T}
-    data::Matrix{I}
-
-    function BinaryConfusionMatrix(classes)
-        if length(classes) != 2
-            throw(DimensionMismatch("BinaryConfusionMatrix is defined only for 2D problems: got $(length(classes))D input. Use ConfusionMatrix instead."))
-        end
-        cls = (classes...,)
-        return new{2,eltype(cls),Int}(cls, zeros(Int, 2, 2))
-    end
-end
+Base.size(C::AbstractConfusionMatrix) = size(C.data)
+Base.getindex(C::AbstractConfusionMatrix, I::Vararg{Int,2}) = getindex(C.data, I...)
+Base.setindex!(C::AbstractConfusionMatrix, v, I::Vararg{Int,2}) = setindex!(C.data, v, I...)
 
 function Base.show(io::IO, ::MIME"text/plain", C::AbstractConfusionMatrix)
     N = size(C.data, 2)
@@ -60,12 +40,6 @@ function Base.show(io::IO, ::MIME"text/plain", C::AbstractConfusionMatrix)
     )
 end
 
-Base.size(C::AbstractConfusionMatrix) = size(C.data)
-Base.zero(C::ConfusionMatrix) = ConfusionMatrix(C.classes)
-Base.zero(C::BinaryConfusionMatrix) = BinaryConfusionMatrix(C.classes)
-Base.getindex(C::AbstractConfusionMatrix, I::Vararg{Int,2}) = getindex(C.data, I...)
-Base.setindex!(C::AbstractConfusionMatrix, v, I::Vararg{Int,2}) = setindex!(C.data, v, I...)
-
 function Base.:(+)(C1::AbstractConfusionMatrix, C2::AbstractConfusionMatrix)
     cls1 = C1.classes
     cls2 = C2.classes
@@ -84,14 +58,152 @@ function Base.:(+)(C1::AbstractConfusionMatrix, C2::AbstractConfusionMatrix)
     end
 end
 
+"""
+    ConfusionMatrix{N,T,I<:Integer} <: AbstractConfusionMatrix{I}
+
+Represenation of a `NxN` confusion matrix with classes of type `T` and elements of type `I`. Rows represent target (or ground truth) and columns represent predictions.
+
+For more information see [`confusion`](@ref) function.
+
+# Fields
+- `classes::NTuple{N,T}`: tuple of classes
+- `data::Matrix{I}`: confusion matrix
+"""
+struct ConfusionMatrix{N,T,I<:Integer} <: AbstractConfusionMatrix{I}
+    classes::NTuple{N,T}
+    data::Matrix{I}
+
+    function ConfusionMatrix(classes::NTuple{N,T}, data::Matrix{I}) where {N,T,I<:Integer}
+        if size(data) != (N, N)
+            throw(DimensionMismatch("Number of classes must match number of rows and columns of confusion matrix: got length(classes) = $(N) and size(data) = $(size(data) )"))
+        end
+        return new{N,T,I}(classes, data)
+    end
+
+    ConfusionMatrix(classes, data) = ConfusionMatrix((classes...,), Matrix(data))
+end
+
+function ConfusionMatrix(classes)
+    N = length(classes)
+    return ConfusionMatrix(classes, zeros(Int, N, N))
+end
+
+Base.zero(C::ConfusionMatrix) = ConfusionMatrix(C.classes)
+
+"""
+    BinaryConfusionMatrix{T,I<:Integer} <: AbstractConfusionMatrix{I}
+
+Special representation of `2x2` confusion matrix with classes of type `T` and elements of type `I`. Rows represent target (or ground truth) and columns represent predictions. 
+
+For more information see [`confusion`](@ref) function.
+
+# Fields
+- `classes::NTuple{2,T}`: tuple of classes
+- `data::Matrix{I}`: confusion matrix
+"""
+struct BinaryConfusionMatrix{T,I<:Integer} <: AbstractConfusionMatrix{I}
+    classes::NTuple{2,T}
+    data::Matrix{I}
+
+    function BinaryConfusionMatrix(
+        classes::NTuple{2,T},
+        data::Matrix{I}
+    ) where {T,I<:Integer}
+
+        if size(data) != (2, 2)
+            throw(DimensionMismatch("Number of classes must match confusion matrix size: got length(classes) = $(2) and size(data) = $(size(data) )"))
+        end
+        return new{T,I}(classes, data)
+    end
+
+    function BinaryConfusionMatrix(classes, data)
+        return BinaryConfusionMatrix((classes...,), Matrix(data))
+    end
+end
+
+function BinaryConfusionMatrix(classes)
+    N = length(classes)
+    return BinaryConfusionMatrix(classes, zeros(Int, N, N))
+end
+
+Base.zero(C::BinaryConfusionMatrix) = BinaryConfusionMatrix(C.classes)
+
+# Constructors
 labelmap(C::AbstractConfusionMatrix) = labelmap(C.classes)
 labelmap(classes) = ImmutableDict(Pair.(classes, eachindex(classes))...)
 
+"""
+    confusion(y, ŷ)
+
+General constructor for [`ConfusionMatrix`](@ref) and [`BinaryConfusionMatrix`](@ref).
+
+# keyword arguments
+- `classes`: vector-like object of classes. Default value is `sort(unique(y))`.
+- `binary::Bool`: if `true` the function returns BinaryConfusionMatrix in case of a binary problem. Default value is `true`.
+- `target_transform`: function that is aplied element-wisely to transform targets `y` to correspond to given classes, i.e. it must hold `target_transform(y[i]) ∈ classes` for all indices. Default transform is `identity`.
+- `predict_transform`: function that is aplied element-wisely to transform predictions `ŷ` to correspond to given classes, i.e. it must hold `predict_transform(y[i]) ∈ classes` for all indices. Default transform is `identity`.
+
+# Examples
+
+The function can be used to create confusion matrix directly from targets and predictions
+
+```jldoctest
+julia> y = [0, 1, 1, 0, 1, 0, 1, 1, 0, 1];
+
+julia> ŷ = [0, 1, 0, 1, 1, 0, 0, 0, 1, 1];
+
+julia> confusion(y, ŷ)
+2×2 BinaryConfusionMatrix{Int64, Int64}
+┌─────────────────┬──────┐
+│ Predictions ŷ → │ 0  1 │
+│   Targets y ↓   │      │
+├─────────────────┼──────┤
+│        0        │ 2  2 │
+│        1        │ 3  3 │
+└─────────────────┴──────┘
+```
+or for example to create confusion matrix directly from targets, scores and decision threshold
+
+```jldoctest
+julia> y = [0, 1, 1, 0, 1, 0, 1, 1, 0, 1];
+
+julia> s = [0.4, 0.7, 0.2, 0.6, 0.8, 0.4, 0.5, 0.3, 0.9, 0.7];
+
+julia> t = 0.6;
+
+julia> confusion(y, ŷ; predict_transform = s -> s >= t)
+2×2 BinaryConfusionMatrix{Int64, Int64}
+┌─────────────────┬──────┐
+│ Predictions ŷ → │ 0  1 │
+│   Targets y ↓   │      │
+├─────────────────┼──────┤
+│        0        │ 2  2 │
+│        1        │ 3  3 │
+└─────────────────┴──────┘
+```
+Similarly, the function can be used to create multi-class confusion matrix
+```jldoctest
+julia> y = [:a, :b, :b, :c, :b, :a, :c, :a, :b, :c];
+
+julia> ŷ = [:b, :a, :b, :c, :c, :a, :b, :a, :c, :c];
+
+julia> confusion(y, ŷ)
+3×3 ConfusionMatrix{3, Symbol, Int64}
+┌─────────────────┬─────────┐
+│ Predictions ŷ → │ a  b  c │
+│   Targets y ↓   │         │
+├─────────────────┼─────────┤
+│        a        │ 2  1  0 │
+│        b        │ 1  1  2 │
+│        c        │ 0  1  2 │
+└─────────────────┴─────────┘
+```
+"""
 function confusion(
     y,
-    ŷ,
-    classes = sort(unique(y));
-    binary::Bool = length(classes) == 2,
+    ŷ;
+    classes = sort(unique(y)),
+    binary::Bool = true,
     target_transform = identity,
     predict_transform = identity
 )
@@ -102,7 +214,11 @@ function confusion(
     if length(y) != length(ŷ)
         throw(DimensionMismatch("Inconsistent lengths of y and ŷ."))
     end
-    C = binary ? BinaryConfusionMatrix(classes) : ConfusionMatrix(classes)
+    if binary && length(classes) == 2
+        C = BinaryConfusionMatrix(classes)
+    else
+        C = ConfusionMatrix(classes)
+    end
     mapping = labelmap(C)
 
     for (y_i, ŷ_i) in zip(y, ŷ)
@@ -113,21 +229,53 @@ function confusion(
     return C
 end
 
+"""
+    confusion(y::AbstractMatrix, ŷ::AbstractMatrix)
+
+Constructor for [`ConfusionMatrix`](@ref) and [`BinaryConfusionMatrix`](@ref) for cases where both targets and predictions are in a form of matrix, for example when the one-hot encoding is used.
+
+# Examples
+
+```jldoctest
+julia> y = [
+           1 0 0 0 0 1 0 1 0 0
+           0 1 1 0 1 0 0 0 1 0
+           0 0 0 1 0 0 1 0 0 1
+       ];
+
+julia> ŷ = [
+           0 1 0 0 0 1 0 1 0 0
+           1 0 1 0 0 0 1 0 0 0
+           0 0 0 1 1 0 0 0 1 1
+       ];
+
+julia> confusion(y, ŷ)
+3×3 ConfusionMatrix{3, Int64, Int64}
+┌─────────────────┬─────────┐
+│ Predictions ŷ → │ 1  2  3 │
+│   Targets y ↓   │         │
+├─────────────────┼─────────┤
+│        1        │ 2  1  0 │
+│        2        │ 1  1  2 │
+│        3        │ 0  1  2 │
+└─────────────────┴─────────┘
+```
+"""
 function confusion(
     y::AbstractMatrix,
     ŷ::AbstractMatrix;
     obsdim = 2,
+    binary::Bool = true,
     target_transform = argmax,
     predict_transform = argmax,
-    kwargs...
 )
 
     return confusion(
         eachslice(y; dims = obsdim),
-        eachslice(ŷ; dims = obsdim),
-        1:size(y, obsdim == 1 ? 2 : 1);
+        eachslice(ŷ; dims = obsdim);
+        classes = 1:size(y, obsdim == 1 ? 2 : 1),
+        binary,
         target_transform,
         predict_transform,
-        kwargs...
     )
 end
